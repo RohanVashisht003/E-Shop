e426592caade74611d8c5575e7a24b276e0f837a
RohanVashisht003 <sharmarohan544@gmail.com>
2023-09-09 22:51:14 +0530

    Feat: initial setup with homepage and header with products component.

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCnAoqyDCsoV6lwYh7zBDp9OUkhUoPAsXs",
+  authDomain: "e-shop-5cc19.firebaseapp.com",
+  projectId: "e-shop-5cc19",
+  storageBucket: "e-shop-5cc19.appspot.com",
+  messagingSenderId: "854447995680",
+  appId: "1:854447995680:web:881cca945004f56213f545",
+};
